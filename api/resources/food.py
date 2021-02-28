dd26be13352d9dc4922f820d48bc1b606b6d2cb5
Alexander Lee <45320797+leeaj6@users.noreply.github.com>
2021-02-28 16:13:57 -0500

    Added class for food recommendation

--- api/resources/food.py
+++ api/resources/food.py
@@ -36,7 +36,6 @@ class SearchFood(Resource):
     def post(self):
         try:
             body = request.json
-            print(body)
             if body:
             	food = Food.objects(__raw__={ '$text': {'$search' : body['query'] } }).to_json()
             	resp_dict = json.loads(food)
@@ -47,5 +46,35 @@ class SearchFood(Resource):
             			}, 200
             else:
             	return {"error": "Invalid Search"}, 400
+        except Exception as e:
+            raise e
+
+class RecommendFood(Resource):
+    def post(self):
+        try:
+            body = request.json
+            if body:
+                recommendation_params = {}
+                for filter_p in body.keys():
+                    if filter_p == 'allergies':
+                        recommendation_params['allergies'] = {'$nin': body['allergies']}
+                    elif filter_p == 'nutrition':
+                        for n in body['nutrition'].keys():
+                            recommendation_params['nutrition.'+n] = {'$lte': body['nutrition'][n]}
+                    elif filter_p == 'budget':
+                        recommendation_params['price'] = {'$lte': body['budget']}
+                    elif filter_p == 'preferences':
+                        recommendation_params['cuisine'] = {'$in': body['preferences']}
+                    elif filter_p == 'breakfast':
+                        recommendation_params['breakfast'] = {'$eq': body['breakfast']}
+                food = Food.objects(__raw__=recommendation_params).to_json()
+                resp_dict = json.loads(food)
+                return {
+                        "success": True,
+                        "count": len(resp_dict),
+                        "data": resp_dict
+                        }, 200
+            else:
+                return {"error": "Invalid Query"}, 400
         except Exception as e:
             raise e
