3ccddf583489006691f8df5c6a188431c75d5827
Alexander Lee <45320797+leeaj6@users.noreply.github.com>
2021-02-28 16:14:30 -0500

    New Route

--- api/resources/routes.py
+++ api/resources/routes.py
@@ -1,6 +1,7 @@
-from .food import AllFoods, Restaurant, SearchFood
+from .food import AllFoods, Restaurant, SearchFood, RecommendFood
 
 def initialize_routes(api):
 	api.add_resource(AllFoods, '/api/v1/foods')
 	api.add_resource(Restaurant, '/api/v1/foods/restaurant/<restaurant>')
 	api.add_resource(SearchFood, '/api/v1/foods/search')
+	api.add_resource(RecommendFood, '/api/v1/foods/recommendations')
